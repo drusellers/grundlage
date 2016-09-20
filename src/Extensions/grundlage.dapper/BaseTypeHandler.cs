0a3d62186e8a2778a19bf32423010f08481f8984
Dru Sellers <dru@drusellers.com>
2016-09-20 08:57:49 -0500

    Add dapper support

---
+++ src/Extensions/grundlage.dapper/BaseTypeHandler.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Data;
+using Dapper;
+
+namespace grundlage.dapper
+{
+    public abstract class BaseTypeHandler<TValueType, TPrimitive>
+        : SqlMapper.ITypeHandler
+        where TValueType : class, ValueType<TPrimitive>
+        where TPrimitive : IComparable<TPrimitive>, IEquatable<TPrimitive>, IComparable
+    {
+        public void SetValue(IDbDataParameter parameter, object value)
+        {
+            parameter.DbType = MapToDbType();
+            if (value == null || value is DBNull || ((value as TValueType) == null))
+            {
+                parameter.Value = DBNull.Value;
+            }
+            else
+            {
+                var v = (TValueType)value;
+                SetDbParam(parameter, v);
+            }
+        }
+
+        public object Parse(Type destinationType, object value)
+        {
+            if (value == null) return null;
+            if (value == DBNull.Value) return ValueWhenDatabaseIsNull();
+
+            var primitiveValue = (TPrimitive)Convert.ChangeType(value, typeof(TPrimitive));
+            return ConstructInstance(primitiveValue);
+        }
+
+        protected virtual DbType MapToDbType()
+        {
+            var typeMap = DapperHelper.TypeMap;
+
+            return typeMap[typeof(TPrimitive)];
+        }
+
+        protected virtual TValueType ConstructInstance(TPrimitive value)
+        {
+            return Activator.CreateInstance(typeof(TValueType), value) as TValueType;
+        }
+
+        protected virtual void SetDbParam(IDbDataParameter parameter, TValueType obj)
+        {
+            parameter.Value = obj.Unwrap();
+        }
+
+        protected virtual TValueType ValueWhenDatabaseIsNull()
+        {
+            return null;
+        }
+    }
+}
---
+++ src/Extensions/grundlage.dapper/DapperHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Reflection;
+using Dapper;
+
+namespace grundlage.dapper
+{
+    public static class DapperHelper
+    {
+        static readonly FieldInfo Fi = typeof(SqlMapper).GetField("typeMap", BindingFlags.Static
+                                                                             | BindingFlags.GetField
+                                                                             | BindingFlags.NonPublic);
+        public static Dictionary<Type, DbType> TypeMap
+        {
+            get
+            {
+                var typeMap = (Dictionary<Type, DbType>)Fi.GetValue(null);
+
+                return typeMap;
+            }
+        }
+    }
+}
